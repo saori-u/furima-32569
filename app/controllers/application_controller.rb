18a684cb9395919a8235e15a7ebe93e971e182ec
saori-u <74886643+saori-u@users.noreply.github.com>
2020-12-24 16:02:22 +0900

    コントローラーの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :email, :password, :family_name, :given_name, :family_name_kana, :given_name_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :family_name, :given_name, :family_name_kana, :given_name_kana, :birthday])
   end
 end
