bc8b1dfed2a0e845b7268cce90b5a53a206a4a71
saori-u <74886643+saori-u@users.noreply.github.com>
2020-12-29 18:40:07 +0900

    不要コード(とファイルの削除

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,17 +1,12 @@
 window.addEventListener("load",()=>{
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    console.log(inputValue);
-
-    const addTaxDom = document.getElementById("add-tax-price")
-    const addTaxValue = (Math.floor(inputValue * 0.1));
-    addTaxDom.innerHTML = addTaxValue;
-    console.log(addTaxDom);
-    
-    const profitNumber = document.getElementById("profit")
+    const inputValue       = priceInput.value;
+    const addTaxDom        = document.getElementById("add-tax-price")
+    const addTaxValue      = (Math.floor(inputValue * 0.1));
+    addTaxDom.innerHTML    = addTaxValue;
+    const profitNumber     = document.getElementById("profit")
     profitNumber.innerHTML = (Math.floor(inputValue - addTaxValue));
-      console.log(profitNumber);
       
 });
 })
