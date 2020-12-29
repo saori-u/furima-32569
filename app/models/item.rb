b3854858f6e21592e4ebe2cc1092cdc088a19fdd
saori-u <74886643+saori-u@users.noreply.github.com>
2020-12-29 18:00:56 +0900

    商品出品機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,28 @@
 class Item < ApplicationRecord
-  belongs to :users
+  belongs_to        :user
+  has_one_attached  :image
+
+  with_options presence: true do
+    validates  :image
+    validates  :name
+    validates  :text
+    validates  :price
+  end
+    
+    validates    :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+    validates    :price, numericality: { message: 'Half-width number' }
+
+  with_options numericality: { other_than: 1, message: 'Select' } do
+    validates  :category_id
+    validates  :condition_id
+    validates  :shipping_charge_id
+    validates  :area_id
+    validates  :delivery_time_id
+  end
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :area
+  belongs_to_active_hash :delivery_time
 end
--- db/migrate/20201224112055_create_items.rb
+++ db/migrate/20201224112055_create_items.rb
@@ -1,11 +1,11 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
-    create_table :items do |t|
+    create_table   :items do |t|
       t.string     :name,                  null: false
       t.text       :text,                  null: false
       t.integer    :category_id,           null: false
       t.integer    :condition_id,          null: false
-      t.integer    :shipping_charges_id ,  null: false
+      t.integer    :shipping_charge_id ,   null: false
       t.integer    :area_id,               null: false
       t.integer    :delivery_time_id,      null: false
       t.integer    :price,                 null: false
