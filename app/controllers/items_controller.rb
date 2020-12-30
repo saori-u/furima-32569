4e1f6e88c3dcaabe3c580a7099b9d5295643f152
saori-u <74886643+saori-u@users.noreply.github.com>
2020-12-30 15:35:26 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new ]
+  before_action :authenticate_user!, only: [:index, :show]
   
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -16,6 +16,14 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    redirect_to root_path if item.destroy
   end
 
   private
